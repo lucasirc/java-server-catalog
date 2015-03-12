be2b8867bfdc5d7a3aea593ed74b11b925cf9bbd
Lucas Castro <lucasirc@gmail.com>
2015-03-12 00:15:19 -0300

    +adicionado configuração de log

--- src/main/java/com/lucasirc/servercatalog/core/ServerCatalog.java
+++ src/main/java/com/lucasirc/servercatalog/core/ServerCatalog.java
@@ -6,25 +6,27 @@ import de.caluga.morphium.Morphium;
 import de.caluga.morphium.MorphiumConfig;
 import de.caluga.morphium.MorphiumSingleton;
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 
 
 public class ServerCatalog {
 
-
+    public static final Logger log = Logger.getLogger(ServerCatalog.class);
     public static final String host = "localhost";
     public static final int port = 8080;
     public static final String link = "http://" + host + ":" + port+"/";
-    public static Morphium morphium;
 
     public static void main(String[] args) throws IOException {
+        configureLogging();
         connectDb();
-        BasicConfigurator.configure();
         HttpServer server = startServer();
 
         System.in.read();
@@ -32,14 +34,14 @@ public class ServerCatalog {
     }
 
     public static HttpServer startServer() {
-        System.out.println("Init Server...");
+        log.info("Init Server...");
 
         URI uri = URI.create(link);
         ResourceConfig config = new ResourceConfig().packages("com.lucasirc.servercatalog");
 
         HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
 
-        System.out.println("Server running: "+ link);
+        log.info("Server running: "+ link);
         return server;
     }
     public static void connectDb() {
@@ -54,8 +56,23 @@ public class ServerCatalog {
                 MorphiumSingleton.get();
             }
         }catch (Exception e ) {
+            log.error("Erro ao inicar conexao com banco", e);
             e.printStackTrace();
             System.exit(1);
         }
+    }
+    public static void configureLogging() {
+        //BasicConfigurator.configure();
+
+        try {
+            InputStream config = ServerCatalog.class.getClassLoader().getResourceAsStream("log4j.properties");
+            PropertyConfigurator.configure(config);
+
+        } catch(Exception e){
+            BasicConfigurator.configure();
+            log.error("Erro ao iniciar configuração de log", e);
+        }
+
+
     }
 }
