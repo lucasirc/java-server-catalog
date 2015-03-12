0a573da95247d7a70ed552deb0e02bf2c17f2bde
Lucas Castro <lucasirc@gmail.com>
2015-03-11 23:35:20 -0300

    + delete para applicationResource + alterado list do appResource para ter max e offset

--- src/main/java/com/lucasirc/servercatalog/resources/ApplicationResource.java
+++ src/main/java/com/lucasirc/servercatalog/resources/ApplicationResource.java
@@ -1,16 +1,19 @@
 package com.lucasirc.servercatalog.resources;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.lucasirc.servercatalog.model.Application;
 import com.lucasirc.servercatalog.model.Server;
 import com.lucasirc.servercatalog.service.ApplicationResourceService;
 import com.lucasirc.servercatalog.service.ResourceFactory;
+import com.lucasirc.servercatalog.service.ServerResourceService;
 
 import javax.ws.rs.*;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
+import java.util.List;
 import java.util.Map;
 
 @Path("{version}/applications")
@@ -25,6 +28,14 @@ public class ApplicationResource {
     public Response get(@PathParam("id") long id) {
         System.out.println("Appls: " + id);
         Map appMap = getAppResourceService().get(id);
+
+
+        if (appMap == null) {
+            JsonObject json = new JsonObject();
+            json.addProperty("msg", id + " not found!");
+
+            return Response.status(Response.Status.NOT_FOUND).entity(json.toString()).build();
+        }
 
         String json = new Gson().toJson(appMap);
         return Response.ok(json, MediaType.APPLICATION_JSON).build();
@@ -43,6 +54,31 @@ public class ApplicationResource {
 
             return Response.serverError().build();
         }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response list(@QueryParam("offset") long offset,
+                         @QueryParam("max") long max) {
+
+        offset = offset != 0 ? offset : 0;
+        max = Math.min(max != 0 ? max : 10, 100);
+
+        List list = getAppResourceService().list(offset, max);
+
+        String json = new Gson().toJson(list);
+        return Response.ok(json, MediaType.APPLICATION_JSON).build();
+    }
+
+
+    @DELETE
+    @Path("{id}")
+    public Response delete(@PathParam("id") long id){
+        ApplicationResourceService service = getAppResourceService();
+
+        service.delete(id);
+
+        return Response.ok().build();
     }
 
     public ApplicationResourceService getAppResourceService() {
