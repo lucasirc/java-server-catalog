7730e6306604a6df0fb2450e7237d527872cbf74
Lucas Castro <lucasirc@gmail.com>
2015-03-12 21:27:09 -0300

    + utilizando log4j

--- src/main/java/com/lucasirc/servercatalog/service/ResourceFactory.java
+++ src/main/java/com/lucasirc/servercatalog/service/ResourceFactory.java
@@ -2,13 +2,16 @@ package com.lucasirc.servercatalog.service;
 
 import com.lucasirc.servercatalog.service.v1.ApplicationV1Transformer;
 import com.lucasirc.servercatalog.service.v1.ServerV1Transformer;
+import org.apache.log4j.Logger;
 
 public class ResourceFactory {
+
+    public static final Logger log = Logger.getLogger(ResourceFactory.class);
 
     public static final String DEFAULT_VERSION = "v1";
 
     public static ServerResourceService getServerService(String version) {
-        System.out.println("Server Version: " + version);
+        log.debug("Server Version: " + version);
         if ( version == null || DEFAULT_VERSION.equals(version)) {
             ServerV1Transformer transformer = new ServerV1Transformer();
             return new ServerResourceService(transformer);
@@ -17,7 +20,7 @@ public class ResourceFactory {
     }
 
     public static ApplicationResourceService getAppResourceService(String version) {
-        System.out.println("Application Version: " + version);
+        log.debug("Application Version: " + version);
         if ( version == null || DEFAULT_VERSION.equals(version)) {
             ApplicationV1Transformer transformer = new ApplicationV1Transformer();
             return new ApplicationResourceService(transformer);
